ded841f02fd04c93ed153e3cc3663e62784fd0cf
Derock <derock@derock.dev>
2023-02-27 09:24:00 -0500

    feat: copilot tab completion & update to astronvim 3

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AstroNvim Configuration
 astronvim seems like a good base neovim configuration so i am using that. this repo holds my `~/.config/nvim/lua/user` files which are used to further customize astronvim.
 
+**Requires AstroNvim v3 (nightly)**
+
 ## Contents
 pretty empty right now, working to get copilot and stuff working.
--- init.lua
+++ init.lua
@@ -1,56 +1,42 @@
 return {
   lsp = {
     ["server-settings"] = {
-      clangd = { 
-        capabilities = { 
-          offsetEncoding = "utf-8" 
-        } 
+      clangd = {
+        capabilities = {
+          offsetEncoding = "utf-8"
+        }
       }
     }
   },
-
   plugins = {
-    init = {
-      -- Copilot.nvim
-      -- run :Copilot setup to authenticate 
-      { 
-        "github/copilot.vim",
-      },
-     -- JSDoc stuff
-      {
-        "kkoomen/vim-doge",
-      }
+    -- Copilot.lua
+    {
+      "AstroNvim/astrocommunity",
+      { import = "astrocommunity.completion.copilot-lua-cmp" }
     },
 
     -- Customize GitSigns
     -- I like how VSC's GitLens shows current line blame
-    gitsigns = {
-      current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 200
+    {
+      "lewis6991/gitsigns.nvim",
+      opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 200
+        }
       }
     }
   },
-
   mappings = {
     -- Normal mode
     n = {
       -- Toggle terminal - same keybind as VSC
       ["<C-j>"] = { "<cmd>ToggleTermToggleAll<cr>", desc = "Hide all terms" },
     },
-  
+
     -- Insert mode
     i = {
-      ["<C-e>"] = { "copilot#Accept('<CR>')", silent = true, expr = true },
       ["<C-j>"] = { "<cmd>ToggleTermToggleAll<cr>", desc = "Hide all terms" },
     },
   },
-
-  options = {
-    g = {
-      copilot_no_tab_map = true,
-      copilot_assume_mapped = true,
-      copilot_tab_fallback = true
-    }
-  }
 }
